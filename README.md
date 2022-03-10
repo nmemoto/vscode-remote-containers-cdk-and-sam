bc4cbf6e135a54a9518aab701fffc9c41132fc42
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2022-03-10 03:41:45 +0000

    CDKプロジェクトを追加

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vscode-remote-containers-cdk-and-sam
+
+VSCode Remote Containers でAWS CDK と AWS SAM を使いコンテナ内部でLambdaを実行する
+
+```bash
+cd app
+cdk synth --no-staging
+sam local invoke AppStack-Function --no-event -t ./cdk.out/AppStack.template.json --container-host host.docker.internal
+```
---
+++ app/lib/app-stack.ts
@@ -0,0 +1,26 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AppQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const fn = new Function(this, `${this.stackName}-Function`, {
+      functionName: `${this.stackName}-Function`,
+      code: Code.fromAsset('./lambda/my_function'),
+      runtime: Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      timeout: cdk.Duration.seconds(3),
+    })
+  }
+}
